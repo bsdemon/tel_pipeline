56d962a6bc976793e31441d98a736ee0efcf8c76
Iliya Iliev <iliya.iliev@x3me.net>
2025-01-26 16:19:13 +0200

    Fix tests

--- app/app_test.py
+++ app/app_test.py
@@ -7,10 +7,10 @@ class TestApp(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
 
-    def test_hello_world(self):
+    def test_home_route(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b"Hello, World!")
+        self.assertEqual(response.data, b"App is running!")
 
     def test_rediness(self):
         response = self.client.get("/health/ready")
